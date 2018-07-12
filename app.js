2dc7324c33faac3266c5ce4711699dd5c2d053d0
Juliet Urbina <julieturbina8@gmail.com>
2018-07-11 21:53:12 -0400

    facebook login

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session      = require('express-session');
+const bcrypt       = require("bcrypt");
+const passport     = require("passport");
+const LocalStrategy= require("passport-local").Strategy;
+const User         = require("./models/user");
+const flash        = require("connect-flash");
+const FbStrategy = require('passport-facebook').Strategy;
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/slick-shopper-app', {useMongoClient: true})
+  .connect('mongodb://localhost/solialite', {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
@@ -29,6 +35,42 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "the-socialite-app",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//facebook
+passport.use(new FbStrategy({
+  clientID: "facebookId",
+  clientSecret: "secretKey",
+  callbackURL: "/auth/facebook/callback"
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOne({ facebookID: profile.id }, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    if (user) {
+      return done(null, user);
+    }
+
+    const newUser = new User({
+      facebookID: profile.id
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, newUser);
+    });
+  });
+
+}));
+// facebook end
 
 // Express View engine setup
 
@@ -44,6 +86,37 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+app.use(flash());
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+  },(req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username!!!" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // default value for title local
@@ -52,7 +125,10 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
+const authRoutes = require("./routes/auth-routes");
+
 app.use('/', index);
+app.use('/', authRoutes);
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
+
 const express = require('express');
 const router  = express.Router();
-
-// movie project below==========
-// const Movie = require('../models/movie.js');
-// const uploadCloud = require('../config/cloudinary.js');
-// movie project above ===============
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -13,39 +9,3 @@ router.get('/', (req, res, next) => {
 
 module.exports = router;
 
-
-
-
-
-/* mpvie project below========
-/* GET home page */
-// router.get('/', (req, res, next) => {
-//   Movie.find()
-//   .then((movies) => {
-//     res.render('index', { movies });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-// });
-
-// router.get('/movie/add', (req, res, next) => {
-//   res.render('movie-add');
-// });
-
-// router.post('/movie/add', uploadCloud.single('photo'), (req, res, next) => {
-//   const { title, description } = req.body;
-//   const imgPath = req.file.url;
-//   const imgName = req.file.originalname;
-//   const newMovie = new Movie({title, description, imgPath, imgName})
-//   newMovie.save()
-//   .then(movie => {
-//     res.redirect('/');
-//   })
-//   .catch(error => {
-//     console.log(error)
-//   })
-// });
-
-// module.exports = router;
-// */ movie project above =========
